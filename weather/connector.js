d30ee8ad42883bd7bfa8b14156803f627003ace1
iskander <iskandercapital@gmail.com>
2022-11-29 13:49:54 +0300

    update connector

--- weather/connector.js
+++ weather/connector.js
@@ -2,6 +2,8 @@ import { search } from "./CONTROLLER/handlers.js";
 import { render, cityRender } from "./VIEW/render.js";
 import { getCityInfo } from "./MODEL/getcity.js";
 render(getCityInfo("Aktobe"));
-cityRender(JSON.parse(localStorage.getItem("cities")));
+if (localStorage.getItem("cities")) {
+  cityRender(JSON.parse(localStorage.getItem("cities")));
+}
 const searchForm = document.querySelector(".main__city-search");
 searchForm.addEventListener("submit", search);
